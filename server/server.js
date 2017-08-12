43e68a45acf6b60eea4d6a28dfc5d8378e5c7d54
Suliluz Marcus Marrio <marriomarcuslogin@gmail.com>
2017-08-12 19:19:39 +0800

    Add test for delete /todos

--- server/server.js
+++ server/server.js
@@ -65,7 +65,7 @@ app.delete('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.status(200).send(todo);
+    res.status(200).send({todo});
 
   }).catch((e) => {
     res.status(400).send();
