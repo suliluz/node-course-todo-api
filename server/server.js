bc89eee6df4c8b21167bd1f14d33ae7e4ec31ed8
Suliluz Marcus Marrio <marriomarcuslogin@gmail.com>
2017-08-12 16:26:50 +0800

    Add delete todos by id commit

--- server/server.js
+++ server/server.js
@@ -52,6 +52,26 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var todoId = req.params.id;
+
+  if(!ObjectID.isValid(todoId)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(todoId).then((todo) => {
+
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    res.status(200).send(todo);
+
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
