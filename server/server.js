0f128758b642edceba8ea844ff7a5ed552bf5028
Suliluz Marcus Marrio <marriomarcuslogin@gmail.com>
2017-08-22 14:57:04 +0800

    Add POST /users/login and generate Auth token

--- server/server.js
+++ server/server.js
@@ -105,7 +105,7 @@ app.patch('/todos/:id', (req, res) => {
 
 // POST /users
 app.post('/users', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password'])
+  var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
   user.save().then(() => {
@@ -125,6 +125,19 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+//POST /users/login {email, password}
+app.post('/users/login/', (req, res) => {
+  var body = _.pick(req.body, ['email','password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
